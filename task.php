09a7f1ddae9432a20c5bbd785953c44cdbad9a90
okoye <okoye@wearebeem.com>
2018-02-25 23:45:00 +0000

    Flow diagram upload process

--- task.php
+++ task.php
@@ -8,59 +8,59 @@
 $directoryName = 'uploaded';
 
 if(checkFolder($directoryName)){
-    
+
     echo $directoryName.' folder exists <br/>';
-    
-    
+
+
     //create an array of all csv files
     $files = glob($directoryName."/*.csv");
-    
+
     //check if the array holds any files
     if(count($files) != 0){
-        
+
         tableManage();
-        
-        //looping through the csv files found   
+
+        //looping through the csv files found
        foreach($files as $file){
-        
+
             //print_r(readCSV($file)).'<br/>';
            //loadCSVData($file);
            parseCSVData($file);
-           
+
            $directoryName = 'processed';
 
            if(checkFolder($directoryName)){
-    
+
             moveFile($file);
-           
+
            }
            else
                createFolder($directoryName);
-               
-           
+
+
         }
-        
+
     }
     else
         echo 'No csv files found<br/>';
-    
+
 
 }
 else{
     echo 'false<br/>';
     createFolder($directoryName);
-    //do nothing folder is empty 
+    //do nothing folder is empty
 }
 function checkFolder($name){
-    
+
     return file_exists($name);
 }
 function createFolder($name){
-    
+
      mkdir($name, 0700);
 }
 function readCSV($csvFile){
-    
+
     $file_handle = fopen($csvFile, 'r');
     while (!feof($file_handle)) {
         $line_of_text[] = fgetcsv($file_handle, 1024);
@@ -69,8 +69,8 @@ function readCSV($csvFile){
     return $line_of_text;
 }
 function tableManage(){
-    
-    //If array holds files check if table in db exists 
+
+    //If array holds files check if table in db exists
         $servername = "localhost";
         $username = "root";
         $password = "root";
@@ -83,17 +83,17 @@ function tableManage(){
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
-        
+
         else{
-            
-            // sql to create table if not exist 
+
+            // sql to create table if not exist
             $sql = "CREATE TABLE ".$table." ( `event_id` INT(8) UNSIGNED NOT NULL AUTO_INCREMENT ,`eventDatetime` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP , `eventAction` VARCHAR(20) NOT NULL , `callRef` INT NULL , `eventValue` DECIMAL NULL , `eventCurrencyCode` VARCHAR(3) NULL ,PRIMARY KEY (`event_id`))";
-            
+
         }
 
         if (mysqli_query($conn, $sql)) {
             echo "Table ".$table." created successfully";
-        } 
+        }
         else {
 
             echo "Error creating table: " . mysqli_error($conn);
@@ -101,7 +101,7 @@ function tableManage(){
                 //error is existence of table
                 if($result->num_rows == 1) {
                     echo "Table exists";
-                    //insert data here 
+                    //insert data here
                 }
             }
             else {
@@ -109,7 +109,7 @@ function tableManage(){
             }
         }
 
-        mysqli_close($conn); 
+        mysqli_close($conn);
 }
 function loadCSVData($fileName, $servername = "localhost",
         $username = "root",
@@ -127,7 +127,7 @@ function loadCSVData($fileName, $servername = "localhost",
         IGNORE 1 LINES
         (eventDatetime,eventAction,callRef,eventValue,eventCurrencyCode)
         SET Date = STR_TO_DATE(eventDatetime, '%Y-%m-%d %H:%M:%S')";
-    
+
     echo($query);
 
     // Create connection fix this
@@ -140,30 +140,30 @@ function parseCSVData($fileName, $servername = "localhost",
         $password = "root",
         $dbname = "PHP_Daemon",
         $table = "Daemon"){
-    
+
    $csvFile = fopen($fileName, 'r');
     //skip 1st line
     $head = fgetcsv($csvFile);
-    
+
     for($x=0;$x<count($head);$x++){
-        
+
         print_r($type[$x] = gettype($head[$x]));
-        
+
     }
-    
+
     while(($line = fgetcsv($csvFile)) != false){
         print_r($line);
         /*for($x=0; $x<5; x++){
-            
+
            if(isset($line[x])){
-             
+
             }
             else
-                $line[x] = ''; 
+                $line[x] = '';
             }*/
-        
+
         $query = "INSERT INTO ".$table." ($head[0],$head[1],$head[2],$head[3],$head[4]) VALUES ('".$line[0]."','".$line[1]."','".$line[2]."','".$line[3]."','".$line[4]."')";
-        
+
         // Create connection fix this
         $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -171,14 +171,14 @@ function parseCSVData($fileName, $servername = "localhost",
     }
 }
 function moveFile($fileName){
-    
+
     $old = $fileName;
     //$parentFolder = basename(__DIR__);
     echo $new = "processed/".basename($fileName);
     copy($old, $new) or die("Unable to copy $old to $new.");
     if (copy($old,$new)) {
       unlink($old);
-        echo "<br\>".$fileName."deleted";    
+        echo "<br\>".$fileName."deleted";
     }
 }
 ?>
